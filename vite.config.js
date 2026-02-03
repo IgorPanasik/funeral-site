cadc1e532f043f748d58644496a027bcc34f16e2
IgorPanasik <102683696+IgorPanasik@users.noreply.github.com>
2026-02-03 16:53:52 +0300

    Update build and assets

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { VitePWA } from 'vite-plugin-pwa';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
 	base: '',
@@ -14,4 +17,40 @@ export default defineConfig({
 		},
 	},
 	css: { postcss: './postcss.config.cjs' },
+	plugins: [
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['favicon.ico', 'robots.txt'],
+			manifest: {
+				name: 'Ритуальное бюро | Алая Роза',
+				short_name: 'Алая Роза',
+				description: 'Ритуальные услуги в Минске',
+				theme_color: '#000000',
+				background_color: '#ffffff',
+				display: 'standalone',
+				icons: [
+					{ src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+					{ src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
+				],
+			},
+		}),
+
+		createHtmlPlugin({
+			inject: {
+				data: {
+					title:
+						'Ритуальные услуги в Минске | Похоронное бюро круглосуточное | Алая роза',
+					description:
+						'Заказать ритуальные услуги в похоронном бюро «Алая роза» в Минске и Минском р-не ⭐ Более 10 лет организуем похороны в счёт государственного пособия на погребения ⭐ Круглосуточный вызов агента по телефону +375 (29) 173-17-93 ☎️',
+					ogImage: './preview.jpg',
+					canonical: 'https://funeral.by/',
+				},
+			},
+		}),
+
+		sitemap({
+			hostname: 'https://funeral.by',
+			dynamicRoutes: ['/privacy.html'],
+		}),
+	],
 });
